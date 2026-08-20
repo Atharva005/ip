e181b6cd7c8a649f77f9d1c14b9ea4dac324c9eb
Atharva005 <atharvarege2005@gmail.com>
2026-08-20 23:41:45 +0800

    Level-0

---
+++ src/main/java/Milk.java
@@ -0,0 +1,12 @@
+public class Milk {
+    public static void main(String[] args) {
+        String banner = " __  __   _   _   _        _   _    \n"
+                + "|  \\/  | (_) | | | | __   | | | |\n"
+                + "| |  | | | | | | |   <    |_| |_|   \n"
+                + "|_|  |_| |_| |_| |_|\\_\\   (_) (_)   \n";
+        // used https://www.asciiart.eu/text-to-ascii-art for this!
+        System.out.println(banner);
+        System.out.println("> Milk is here!! What do you need today?");
+        System.out.println("> See you next time!~");
+    }
+}
