ade180fd133efb6cd667fb75937f330e9f958261
Atharva005 <atharvarege2005@gmail.com>
2026-08-25 19:39:25 +0800

    Level-1

--- src/main/java/Milk.java
+++ src/main/java/Milk.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Milk {
     public static void main(String[] args) {
         String banner = " __  __   _   _   _        _   _    \n"
@@ -7,6 +9,15 @@ public class Milk {
         // used https://www.asciiart.eu/text-to-ascii-art for this!
         System.out.println(banner);
         System.out.println("> Milk is here!! What do you need today?");
+
+        Scanner scanner = new Scanner(System.in);
+        String line = scanner.nextLine();
+
+        while (!line.matches("bye")) {
+            System.out.println("> " + line);
+            line = scanner.nextLine();
+        }
+
         System.out.println("> See you next time!~");
     }
 }
